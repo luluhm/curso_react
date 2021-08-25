9792c60a1f6f65875c74bbdc1a7f8c2725815dc4
Maria de Lourdes Hernandez Martinez <luluhm@hotmail.com>
2021-08-25 11:37:14 -0500

    agregamos el repaso

---
+++ hola_mundo_reactJS/src/components/HolaMundo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+    const HolaMundo = () => {
+        return(
+            <div>
+            <h1>Hola mundo</h1>;
+            </div>
+        );          
+    };
+export default HolaMundo
